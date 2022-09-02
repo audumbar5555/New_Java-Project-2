b46204fe10d390eec29a71dfa443067b4fade3ff
Audumbar <Audumbar@LAPTOP-UNTV3BVM>
2022-09-02 12:23:32 +0530

    I have created new java project 2

---
+++ New java project 2/src/javapackage_2/JavaC_2.java
@@ -0,0 +1,13 @@
+package javapackage_2;
+
+public class JavaC_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is Java C2 class");
+		
+		System.out.println("This is selenium C2 class");
+
+	}
+
+}
